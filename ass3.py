bce44b0a6d30f67b71213db49195f7bb5dcb01df
Manas Jadhav <jadhavmanas07@gmail.com>
2026-08-10 22:33:06 +0530

    Modified ass3.py

--- ass3.py
+++ ass3.py
@@ -4,6 +4,8 @@
 number = float(input("Enter a number: "))
 if(number > 0):
     print("The number is positive.")
+elif(number == 0):
+    print("The number is zero.")
 else:
     print("The number is negative.")
     
