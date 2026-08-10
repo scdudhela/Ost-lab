36bf61b43bdb120e5f568b0ffdef7ed5575e6875
swayamdudhela <swayamdudhela@gmail.com>
2026-08-10 22:03:25 +0530

    ADD ass3.py

---
+++ ass3.py
@@ -0,0 +1,9 @@
+# code to determine whether a number is 
+#positive or negative
+
+number = float(input("Enter a number: "))
+if(number > 0):
+    print("The number is positive.")
+else:
+    print("The number is negative.")
+    
